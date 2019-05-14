0a4f4ac244fa7476e6a64b381b9c60290d23d613
Mark, a foreigner <20504032+mark99-19@users.noreply.github.com>
2019-05-14 23:57:12 +0200

    Update Captcha.java

--- src/secondo/Captcha.java
+++ src/secondo/Captcha.java
@@ -1,9 +1,47 @@
 package secondo;
+import java.util.Random;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class Captcha {
 	
+	private String[] domande = new String[3];
+	private String[] pwds = new String[3];
+	int scelta = 0;
+	Random casuale = new Random();
+	
+	private String proponi_domanda()
+	{
+		scelta = casuale.nextInt(2);
+		return domande[scelta];
+	}
+	
+	private boolean controlla_domanda(String challenge)
+	{
+		try {
+			if(challenge.equals(sha1(pwds[scelta])))
+			{
+				return true;
+			}
+		} catch (NoSuchAlgorithmException e) {
+			System.err.println(e);
+		}
+		return false;
+		
+	}
 	
 	
+	private String sha1(String input) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+         
+        return sb.toString();
+    }
 	
 
 }
