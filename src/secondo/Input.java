fa9cc433d9f5ff49358c465d4a4ff0f92d0b81bb
Mark, a foreigner <20504032+mark99-19@users.noreply.github.com>
2019-07-08 18:32:03 +0200

    Aggiunta SQL per Input
    Si evita ridondanza nel corso del progetto

--- src/secondo/Input.java
+++ src/secondo/Input.java
@@ -68,5 +68,29 @@ public class Input {
          
         return sb.toString();
     }
+	
+	public static void connessioneSQL()
+	{
+		//	connessioneSSH();
+		/*	NON FUNZIONA. MAI PIU' DRIVER MANAGER. PER I POSTERI:
+		 * String dbName = "DPO";
+			String dbUserName = "root";
+			String dbPassword = "password";
+			//connessione col server mySql
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//String connectionString = "UPDATE MyTableName " + "SET email = 'ripon.wasim@smile.com' WHERE email='peace@happy.com'";
+			String connectionString = "jdbc:mysql://127.0.0.1:3306/" + dbName + "?user=" + dbUserName + "&password=" + dbPassword + "&useUnicode=true&characterEncoding=UTF-8";  
+			
+		*/	
+			MysqlDataSource dataSource = new MysqlDataSource();
+			dataSource.setUser("root");
+			dataSource.setPassword("password");
+			dataSource.setServerName("milan.onthewifi.com");
+			dataSource.setDatabaseName("DPO");
+			Connection conn = dataSource.getConnection();
+	/*		Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT indirizzo_mail AS mail, parola AS password, token AS token, contatore AS contatore FROM Utenti WHERE (indirizzo_mail = " + hash1 + " AND parola = " + hash2+")");
+	*/
+	}
 
 }
